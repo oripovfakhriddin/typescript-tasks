bfddbcf54f1f10650d7ac566ea6fc4e8f5a34abd
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2023-10-30 15:40:05 +0500

    start

---
+++ homework-1/task-7.ts
@@ -0,0 +1,31 @@
+// type aliases = {
+//   name: string;
+//   isGrant: boolean;
+// };
+
+const seperateStudents = (arr: { name: string; isGrant: boolean }[]): object => {
+  let obj: { grant: string[]; contract: string[] } = {
+    grant: [],
+    contract: [],
+  };
+  for (let i: number = 0; i < arr.length; i++) {
+    if (arr[i].isGrant) {
+      obj.grant.push(arr[i].name);
+    } else {
+      obj.contract.push(arr[i].name)
+    }
+  }
+  return obj;
+};
+
+let students = [
+  { name: "Jurabek", isGrant: false },
+  { name: "Elbek", isGrant: true },
+  { name: "Shamshod", isGrant: false },
+  { name: "Kamola", isGrant: false },
+  { name: "Gulnoza", isGrant: true },
+  { name: "Savlatbek", isGrant: false },
+];
+
+let result = seperateStudents(students);
+console.log(result);
